c02a2188a3f5d7fddfd4f193b3c06e0412016078
root <root@ip-172-31-0-173.us-west-1.compute.internal>
2015-06-11 05:07:02 +0000

    debugged and improved fake_data.py; generate files

--- Ingestion/fake_data.py
+++ Ingestion/fake_data.py
@@ -4,14 +4,17 @@
 # 
 import json
 import random
+import math
 from faker import Factory
 from datetime import datetime
 
 #fake = Factory.create()
 
-print fake.name()
+#print fake.name()
 
-print fake.bs()
+#print fake.bs()
+
+genre_list = ['alternative rock','classical','baroque','romantic','jazz','indie rock','modern','metal','garage rock','grunge','post-punk','electronic','dance','hip hop','rap','pop','80s','90s','hair metal','death metal']
 
 
 def create_fake_freq_dist( freq, genre, wt ):
@@ -32,18 +35,18 @@ def create_fake_freq_dist( freq, genre, wt ):
 
 	"""
 	#for i in range( 1 in fake.int() ):
-	wt = random.random()
+	#wt = random.random()
 	
-	genre = fake.word()
+	#genre = fake.word()
 
-	freq = random.random()	
+	#freq = random.random()	
 	print '{ freq: %s, name: %s, wt: %s }' % (freq,genre,wt)
 	dist = {'freq':freq, 'name': genre, 'wt': wt }
 	#ToDo: generate multiple for a given artist, with decreasing probability
 	#of adding another genre	
 	return json.dumps( dist )
 
-def create_fake_venues(name, city, addr, zip, lat, long, url):
+def create_fake_venues(venue_name, city, addr, zip, lat, long, url):
 	"""Creates list of venues in a city and writes it to file
 			
 
@@ -57,13 +60,13 @@ def create_fake_venues(name, city, addr, zip, lat, long, url):
 
 def create_fake_event(venue, desc, opener, headliner):
 
-	print '{venue: %s, description: %s, opener: %s, headliner: %s}' % (venue, description, opener, headliner)
+	print '{venue: %s, description: %s, opener: %s, headliner: %s}' % (venue, desc, opener, headliner)
 
-	event_info = {'venue': venue, 'description': description, 'opener': opener, 'headliner': headliner }
+	event_info = {'venue': venue, 'description': desc, 'opener': opener, 'headliner': headliner }
 	
 	return json.dumps( event_info )
 
-def fake_it( num_events=10000, num_venues=150, num_artists=500, hrp="/user/sceneFindr/history/"):
+def fake_venues_events( num_events=10000, num_venues=150, num_artists=500, hrp="/user/sceneFindr/history/"):
 	fake = Factory.create()
 	#assuming one city for now.
 	for v in range(num_venues):
@@ -79,12 +82,14 @@ def fake_it( num_events=10000, num_venues=150, num_artists=500, hrp="/user/scene
 
                 venue_name = fake.company()
 
-                url = fake.url()	
+                url = fake.url()
+	
+		city = "San Francisco"	
 
-		venue = create_fake_venues(name, city, addr, zip, lat, long, url)
+		venue = create_fake_venues(venue_name, city, addr, zip, lat, long, url)
 		for event in range(num_events):
 			#each venue has events
-			local_filename = "venue_%s_event_%s.txt" % (v, event)
+			local_filename = "FakeData/venue_%s_event_%s.txt" % (v, event)
 			#open filewriter
         		file_writer = open(local_filename, 'w')
 	
@@ -92,20 +97,47 @@ def fake_it( num_events=10000, num_venues=150, num_artists=500, hrp="/user/scene
 			desc = fake.sentence(nb_words = 6, variable_nb_words = True)
 			
 			opener = fake.name()
-
+			fake_artist_dist( opener )
 			headliner = fake.name()
-	
+			fake_artist_dist( headliner )
 			format = create_fake_event( venue, desc, opener, headliner )
 	
 		
 			#write to file
-			file_writer.write( format  + "\n" )
+			file_writer.write( format  + '\n' )
 			#close the writer
 	       		file_writer.close()
 
 	#put it on HDFS
-#def create_json_
 
-create_fake_freq_dist()
-create_fake_venues('San Francisco')
-create_fake_event('San Francisco')
+def fake_artist_dist( artist ):
+	local_filename = "FakeData/artist_%s_dist.txt" % artist
+	
+	for i in range( len(genre_list) ):
+		print genre_list[i]
+	
+	file_writer = open( local_filename, 'w' )
+	#generate new weight vectors according to a geometric distn
+	#with param lambda = 1/20
+	#sample without replacement from the list of genres
+	#k = math.floor( len( genre_list )/3 )
+	#print k
+	k = 6
+	genres = random.sample( genre_list, k )
+	#while( count < genre_list/2 ):
+	for genre in genres:
+		wt = random.random()
+	
+		#genre = 
+
+		freq = random.random()	
+		format = create_fake_freq_dist( freq, genre, wt )
+		file_writer.write( format + '\n' )
+	
+	file_writer.close()
+	
+
+#create_fake_freq_dist()
+#create_fake_venues('San Francisco')
+#create_fake_event('San Francisco')
+fake_venues_events( num_events=10, num_venues=3, num_artists=20 )
